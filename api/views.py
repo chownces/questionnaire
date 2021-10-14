f117a2b18523dd9e595254d560e4fb763dba83ee
En Rong <53928333+chownces@users.noreply.github.com>
2021-10-14 20:14:36 +0800

    Update viewset endpoints

--- api/views.py
+++ api/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.response import Response
-from rest_framework import status
 
 from .serializers import (
     FormSerializer,
@@ -11,12 +10,24 @@ from .serializers import (
 from .models import Form, Submission
 
 
-class FormViewSet(viewsets.ModelViewSet):
+class FormViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = Form.objects.all().order_by("id")
     serializer_class = FormSerializer
 
 
-class SubmissionViewSet(viewsets.ModelViewSet):
+class SubmissionViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = Submission.objects.all().order_by("form_id")
 
     def get_serializer_class(self):
